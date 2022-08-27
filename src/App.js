b54786cd264d69eb918909709816c7dd85916c77
CamSmall <cam_small@icloud.com>
2022-08-27 14:57:46 -0400

    Completed through "State" section in tutorial.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
-import React, { Component } from 'react'
-import Table from './Table'
+import React, { Component } from "react"
+import Table from "./Table"
 
 class App extends Component {
-    render() {
-        const characters = [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-              },
-              {
-                name: 'Mac',
-                job: 'Bouncer',
-              },
-              {
-                name: 'Dee',
-                job: 'Aspring actress',
-              },
-              {
-                name: 'Dennis',
-                job: 'Bartender',
-              },
-        ]
-        return (
-            <div className="container">
-                <Table characterData={ characters }/>
-            </div>
-        )
-    }
+  state = {
+    characters: [
+      {
+        name: "Cameron",
+
+        job: "Server Admin",
+      },
+      {
+        name: "Jackson",
+        job: "Partner-in-Crime",
+      },
+      {
+        name: "Robert",
+        job: "Technical Developer",
+      },
+      {
+        name: "Gabi",
+        job: "New Player/Camera Account",
+      },
+      {
+        name: "Brian",
+        job: "unassigned",
+      },
+    ],
+  }
+
+  removeCharacter = (index) => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  render() {
+    const { characters } = this.state
+
+    return (
+      <div className='container'>
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+      </div>
+    )
+  }
 }
 
 export default App
